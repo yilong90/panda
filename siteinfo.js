b0b3cbee74b65fabc7c9c063f41779a005e71614
yilong <yilong>
2018-11-23 15:54:55 +0800

    first!

---
+++ siteinfo.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: "萤火小程序商城",
+  // siteroot: "https://www.liyilong.xyz/", // 必填: api地址，结尾要带/
+  siteroot: "http://localhost/panda_php/web/",
+  uniacid: "10001", // 默认即可，勿填
+};
